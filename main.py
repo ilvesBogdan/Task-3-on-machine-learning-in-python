2d14b83004fc3579e1e8e6fbc9e1b1f7d541d1df
ilvesBogdan <116699847+ilvesBogdan@users.noreply.github.com>
2022-11-15 14:27:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def qestion1(i: int) -> str:
+def question1(i: int) -> str:
     """Решение задания 1"""
     result = ' '.join((
         '' if i % 3 else 'Fizz',
@@ -8,7 +8,7 @@ def qestion1(i: int) -> str:
     return str(i) if ' ' == result else result
 
 
-def qestion2(x: int) -> str:
+def question2(x: int) -> str:
     """Решение задания 2"""
     if x % 2:  return "Плохо"
     if x > 20: return "Отлично"
@@ -16,17 +16,17 @@ def qestion2(x: int) -> str:
     if x > 1:  return "Неплохо"
 
 
-def qestion3(x: int) -> str:
+def question3(x: int) -> str:
     """Решение задания 3"""
     return ''.join(map(str, range(1, x + 1)))
 
 
-def qestion4(text: str) -> str:
+def question4(text: str) -> str:
     """Решение задания 4"""
     return ''.join(filter(lambda c: c.isupper(), text))
 
 
-def qestion5(text: str) -> bool:
+def question5(text: str) -> bool:
     """Решение задания 5"""
     for i, word in enumerate(text.split()):
         if word.isnumeric():
@@ -35,15 +35,15 @@ def qestion5(text: str) -> bool:
             return True
 
 
-def qestion6(*argv: str) -> str:
+def question6(*argv: str) -> str:
     """Решение задания 6"""
     return ','.join(map(lambda w: w.replace('right', 'left'), argv))
 
 
 if __name__ == '__main__':
-    print('\n Задание 1:', qestion1(5))
-    print('\n Задание 2:', qestion2(24))
-    print('\n Задание 3:', qestion3(9))
-    print('\n Задание 4:', qestion4("How are you? Eh, ok. Low or Lower? Ohhh."))
-    print('\n Задание 5:', qestion5("1 2 3 4"))
-    print('\n Задание 6:', qestion6("bright aright", "ok"))
+    print('\n Задание 1:', question1(5))
+    print('\n Задание 2:', question2(24))
+    print('\n Задание 3:', question3(9))
+    print('\n Задание 4:', question4("How are you? Eh, ok. Low or Lower? Ohhh."))
+    print('\n Задание 5:', question5("1 2 3 4"))
+    print('\n Задание 6:', question6("bright aright", "ok"))
